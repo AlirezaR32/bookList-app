d90fc0f3fd1b1176d3fe5f60b66af806cb55b195
BlackCap <you@example.com>
2025-07-30 13:54:04 +0330

    use localStorage

--- src/context/BookContext.js
+++ src/context/BookContext.js
@@ -1,18 +1,25 @@
-import react, { createContext, useState } from "react";
+import react, { createContext, useEffect, useState } from "react";
 import {v4 as uuid } from 'uuid';
 
 export const BookContext = createContext();
 const BookContextProvider = (props) => {
-    const [books, setBooks] = useState([
-        { title:'سووشون', author:'سیمین دانشور', id:1},
-        { title:'خوب های بد بدهای خوب', author:'سومان چیمانی', id:2},
-        ])
+    const [books, setBooks] = useState(
+        // [{ title:'سووشون', author:'سیمین دانشور', id:1},
+        // { title:'خوب های بد بدهای خوب', author:'سومان چیمانی', id:2}]
+        () => {
+            const data = localStorage.getItem('books');
+            return data ? JSON.parse(data) : []
+        }
+    )
     const removeBook = (id) => {
         setBooks(books.filter(book => book.id !== id ))
     }
     const addBook = (title ,author) => {
         setBooks([...books,{title,author, id:uuid()}])
     }
+     useEffect(() => {
+        localStorage.setItem('books', JSON.stringify(books))
+    }, [books])
     return (
         <BookContext.Provider value={{books, addBook , removeBook}}>
             {props.children}
